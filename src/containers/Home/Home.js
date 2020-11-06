e1f43327eff0d9fce9b6bb4139a23f702b00e2c2
safeer-shaikh <safeersohail210.ss@gmail.com>
2020-11-06 23:44:30 +0500

    removed extra console

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -3,11 +3,11 @@ import {connect} from 'react-redux'
 import { set_data, facebook_login } from '../../store/Action'
 class Home extends React.Component{
     render(){
-        console.log('home props=>',this.props)
+        // console.log('home props=>',this.props)
         return(
             <div>
                 <h1>Home page</h1>
-                <button onClick={()=>this.props.set_data()} >check console</button>
+                {/* <button onClick={()=>this.props.set_data()} >check console</button> */}
                 <button onClick={()=>this.props.facebook_login()} >Facebook Login</button>
             </div>
         )
@@ -17,7 +17,7 @@ const mapStateToProps = (state) => ({
     users: state.users
 })
 const mapDispatchToProps = (dispatch) => ({
-    set_data: ()=> dispatch(set_data()),
+    // set_data: ()=> dispatch(set_data()),
     facebook_login: ()=> dispatch(facebook_login()),
 })
 export default connect(mapStateToProps,mapDispatchToProps)(Home);
--- src/store/Reducers/index.js
+++ src/store/Reducers/index.js
@@ -7,7 +7,7 @@ const INITIAL_STATE = {
     ]
 }
 export default (state = INITIAL_STATE,action) => {
-    console.log('action==>',action)
+    // console.log('action==>',action)
     switch (action.type){
         case "SETDATA":
             return({
