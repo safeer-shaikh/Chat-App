79dbad02be675906b4ca4e9083c22966ac7bfcc7
safeer-shaikh <safeersohail210.ss@gmail.com>
2020-11-10 23:07:35 +0500

    done with home/login page styling

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import './home.css'
+import {Link} from 'react-router-dom'
+import logoImage from '../../images/logo.jpg'
+import sideImage from '../../images/sideimage.JPG'
 import {connect} from 'react-redux'
 import { set_data, facebook_login } from '../../store/Action'
 class Home extends React.Component{
@@ -8,9 +11,23 @@ class Home extends React.Component{
         return(
             <div className='body'>
                 <div className='sub-body'>
-                    <h1>Home page</h1>
+                    <div className='heading'>
+                        <Link to='/'><img src={logoImage} alt='logo' width='60' height='60' /></Link>
+                        <h3>Talkie Web</h3>
+                    </div>
                     {/* <button onClick={()=>this.props.set_data()} >check console</button> */}
-                    <button onClick={()=>this.props.facebook_login(this.props.history)} >Facebook Login</button>
+                    <div className='content'>
+                        <div className='sub-content1'>
+                            <p>To use Talkie App on your computer: </p>
+                            <button onClick={()=>this.props.facebook_login(this.props.history)}>
+                                <i class="fa fa-facebook-f"></i>
+                                Login with Facebook
+                            </button>
+                        </div>
+                        <div className='sub-content2'>
+                            <img src={sideImage} alt='Chatting Gallery' />
+                        </div>
+                    </div>
                 </div>
             </div>
         )
